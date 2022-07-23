cd53d9e42ead3a9e8b794e3b13884b0b6a425f45
kornkritpawit <konsoo@hotmail.com>
2022-07-23 14:09:05 +0700

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,126 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h3>Payment</h3>
+    <form novalidate @submit.prevent="onSave">
+      <div class="row">
+        <div class="col-md-6">
+          <div><strong>Shipping Information</strong></div>
+          <div class="form-group">
+            <label for="name">Name</label>
+            <input
+              id="name"
+              type="text"
+              class="form-control"
+              placeholder="Your Name"
+              v-model="payment.fullName"
+            />
+          </div>
+          <div class="form-group">
+            <label for="company">Company Name</label>
+            <input
+              id="company"
+              type="text"
+              class="form-control"
+              placeholder="Company"
+              v-model="payment.company"
+            />
+          </div>
+          <div class="form-group">
+            <label for="address1">Address</label>
+            <input
+              id="address1"
+              type="text"
+              class="form-control"
+              placeholder="Street Address"
+              v-model="payment.address1"
+            />
+          </div>
+          <div class="form-group">
+            <label for="address2">Suite/Apartment #</label>
+            <input
+              id="address2"
+              type="text"
+              class="form-control"
+              placeholder=""
+              v-model="payment.address2"
+            />
+          </div>
+          <div class="form-row">
+            <div class="form-group col-md-6">
+              <label for="cityTown">City</label>
+              <input
+                id="cityTown"
+                type="text"
+                class="form-control"
+                placeholder="e.g. New York"
+                v-model="payment.addressTown"
+              />
+            </div>
+            <div class="form-group col-md-3">
+              <label for="stateProvince">State</label>
+              <select
+                id="stateProvince"
+                class="form-control"
+                v-model="payment.stateProvince"
+              >
+                <!-- <option value="">GA</option>
+                <option>CA</option>
+                <option>FL</option>
+                <option>AL</option> -->
+                <option
+                  v-for="s in states"
+                  :key="s.abbreviation"
+                  :value="s.abbreviation"
+                >
+                {{s.name}}
+                  <!-- {{ stateFormat(s) }} -->
+
+                </option>
+              </select>
+            </div>
+            <div class="form-group col-md-3">
+              <label for="postalCode">Zipcode</label>
+              <input
+                id="postalCode"
+                type="text"
+                class="form-control"
+                placeholder="e.g. 10101"
+                v-model="payment.postalCode"
+              />
+            </div>
+            <div class="form-group">
+              <input type="submit" value="Next" class="btn btn-success" />
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6">
+          <div><strong>Billing Information</strong></div>
+        </div>
+      </div>
+    </form>
+    <div>
+      <pre>{{ payment }}</pre>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref, computed, watch } from 'vue';
+import states from "@/lookup/states";
+import formatters from "@/formatters";
+import months from "@/lookup/months";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  setup() {
+    const payment = ref({
+      fullName: 'Shawn',
+      postalCode: '12345',
+    });
+    function onSave() {
+        alert("We can't save yet")
+    //   error.value = "We can't save yet, we don't have an API";
+    }
+    return { payment, states, onSave };
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
